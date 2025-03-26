fb951efadd631d3b5f5eda45d27477f28be460af
Aashish Gahlawat <aashish.gahlawat@stacksville.com>
2025-03-26 12:05:51 +0530

    code refactor

--- api/file.py
+++ api/file.py
@@ -1,24 +1,19 @@
 import boto3
-from botocore.exceptions import NoCredentialsError, PartialCredentialsError
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError, ConnectionError
 from fastapi import File, UploadFile, HTTPException
 
 from config.fastapi import app_settings
-
-# Initialize Boto3 S3 client
-s3_client = boto3.client(
-    "s3",
-    aws_access_key_id=app_settings.S3.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=app_settings.S3.AWS_SECRET_ACCESS_KEY,
-    region_name=app_settings.S3.AWS_REGION,
-)
+from utils.s3 import get_s3_client
 
 
 async def upload_file(file: UploadFile = File(...)):
     try:
-        # Upload the file to S3
+        s3_client = get_s3_client()
         s3_client.upload_fileobj(file.file, app_settings.S3.S3_BUCKET_NAME, file.filename)
         file_url = f"https://{app_settings.S3.S3_BUCKET_NAME}.s3.{app_settings.S3.AWS_REGION}.amazonaws.com/{file.filename}"
         return {"message": "File uploaded successfully", "url": file_url}
+    except ConnectionError:
+        raise HTTPException(status_code=500, detail="Unable to connect to AWS S3")
     except NoCredentialsError:
         raise HTTPException(status_code=500, detail="AWS credentials not found")
     except PartialCredentialsError:
